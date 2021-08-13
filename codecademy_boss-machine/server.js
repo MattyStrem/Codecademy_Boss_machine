d7fc0a9db036dcfab7b3f1ff895853e7b540babf
Mathew Street <mathew.k.street@gmail.com>
2021-08-13 15:35:56 +0100

    minions API started

--- codecademy_boss-machine/server.js
+++ codecademy_boss-machine/server.js
@@ -12,10 +12,10 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
@@ -24,5 +24,10 @@ const apiRouter = require('./server/api');
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, function () {
+    console.log(`CORS-enabled web server is listening on port ${PORT}`);
+  })
+  
 }
+
+
---
+++ codecademy_boss-machine/server/minions.js
@@ -0,0 +1,56 @@
+const minionsRouter = require('express').Router();
+
+module.exports = minionsRouter;
+
+const {
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+} = require('./db')
+
+minionsRouter.param('minionId', (req, res, next, id) => {
+    const minion = getFromDatabaseById('minions', id);
+    if (minion) {
+        req.minion = minion;
+        next()
+    } else {
+        res.status(404).send('Minion not Found!')
+    }
+
+})
+
+//get all minions
+minionsRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('minions'));
+});
+
+//POST new minion
+minionsRouter.post('/', (req, res, next) => {
+    const newMinion = addToDatabase('minions', req.body);
+    res.status(201).send(newMinion);
+})
+
+//get minion by Id
+minionsRouter.get('/:minionId', (req, res, next) => {
+    res.send(req.minion);
+});
+
+//Put update a minion
+minionsRouter.put('./:minionId', (req, res, next) => {
+    let updatedMinion = updateInstanceInDatabase('minions', req.body);
+    res.send(updatedMinion);
+})
+
+//Delete a minion
+minionsRouter('./:minionId', (req, res, next) => {
+    const deletedMinion = deleteFromDatabasebyId('minions', req.params.minionId);
+    if (deletedMinion) {
+        res.status(204)
+    } else {
+        res.status(500)
+    }
+    res.send();
+})
